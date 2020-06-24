5491267e32f86dee0de0ffa268999b053109f1de
John Kim <johnk9000@gmail.com>
2020-06-24 10:28:27 -0500

    Vars complete

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // Operator Variables
 var plusButton = $("#button-plus");
 var minusButton = $('#button-minus');
-var timesButton = $('#button-times');
+var timesButton = $('#button-multiply');
+var divideButton = $('#button-divide');
+var powerButton = $('#button-power');
 var equalsButton = $('#button-equals');
 var clearButton = $('#button-clear');
 
 
 // Number Button Variables
-for
+for(let i = 0; i < 10; i++){
+    var numbers[i] = $('button-' + i + 1);
+}
